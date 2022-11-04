0b7b04a14a0ff34ec533576ee551b7bbc6b911a5
Krupson <krupapiotr500@gmail.com>
2022-11-04 20:24:35 +0100

    putted all comments; enable archive function - all bugs fixed

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//getting all needed elements
 const modalShow = document.querySelector(".info");
 const modal = document.querySelector(".modal-shadow");
 const closeModal = document.querySelector(".close");
@@ -9,16 +10,21 @@ const stopBtn = document.querySelector(".stop");
 const timeList = document.querySelector(".time-list");
 const historyBtn = document.querySelector(".history");
 const resetBtn = document.querySelector(".reset");
-
+//helper variable
 let counter;
 let minutes = 0;
 let seconds = 0;
+//array to showing history
 let timesArr = [];
+//start count function
 const startCount = () => {
+  //hiding history after re-run counter prettier function
   timeList.style.display = "none";
+  //couting function- set interval to helper variable
   counter = setInterval(() => {
     seconds++;
     if (seconds <= 9) {
+      //changing single numbers - simple conditions
       stopwatch.textContent = `${minutes}:0${seconds}`;
     } else if (seconds > 9 && seconds < 59) {
       stopwatch.textContent = `${minutes}:${seconds}`;
@@ -27,32 +33,42 @@ const startCount = () => {
       seconds = 0;
       stopwatch.textContent = `${minutes}:${seconds}`;
     }
+    //set interval as first argument takes a function , second - time when it is to be executed
   }, 100);
 };
-
+//pause function - clear interval -> reason why putted setInterval into variable
 const pauseCounter = () => {
   clearInterval(counter);
 };
+//stop function - clear counter and save last result in time pharagraph.
 const stopWatchFunction = () => {
   clearInterval(counter);
   if (stopwatch.textContent !== "0:00") {
+    //showing pharagraph with subsequent measurement;
     showPauseTime.style.visibility = "visible";
+    //arrange array to store results of subsequent measurements;
     timesArr.push(`Zapis nr ${timesArr.length + 1}: ${stopwatch.textContent}`);
   }
+  //putting values to pharagraph with subsequent measurement;
   showPauseTime.textContent = `Ostatni czas: ${stopwatch.textContent}`;
+  //reseting counter
   minutes = 0;
   seconds = 0;
   stopwatch.textContent = "0:00";
 };
 const historyHandler = () => {
+  //showing a list of last results;
   timeList.style.display = "block";
+  //line to avoid infinite functionality of ARCHIVE button - after printing array of result set defaul UL to empty
   timeList.textContent = "";
+  //forEach loop to create LI elements in UL according to timesArr length
   timesArr.forEach((time) => {
     const newLi = document.createElement("li");
     newLi.textContent = time;
     timeList.appendChild(newLi);
   });
 };
+//reset timer functio - setting all values to 0 or empty and hidding unneded elements.
 const resetHandler = () => {
   timesArr = [];
   minutes = 0;
@@ -63,7 +79,7 @@ const resetHandler = () => {
   showPauseTime.textContent = "0:00";
   showPauseTime.style.visibility = "hidden";
 };
-//showwing help info modal
+//showing help info modal
 modalShow.addEventListener("click", () => {
   modal.style.display = "block";
 });
@@ -71,7 +87,7 @@ modalShow.addEventListener("click", () => {
 closeModal.addEventListener("click", () => {
   modal.style.display = "none";
 });
-
+//all events listeners - all functional buttons
 startBtn.addEventListener("click", startCount);
 pauseBtn.addEventListener("click", pauseCounter);
 stopBtn.addEventListener("click", stopWatchFunction);
